2a0c7de7b5b71fdeea78f37fb9e79eeea4097422
shao <ninedollarmilk@gmail.com>
2021-01-18 00:51:30 -0500

    v8.2 & v8.3

--- file1.py
+++ file1.py
@@ -1,7 +1,11 @@
 print ('Welcome to git')
 print ('Tomorrow is cloudy')
-print ('Function 1: Burton: never used before')
-print ('Function 2: Rome: mod rocker')
-print ('Function 3: Salomon: huck knife')
-print ('Function 4: DC: never used before')
+print ('Board 0: Burton: never used before probablly never will')
+print ('Board 1: SportCheck Board: first board')
+print ('Board 2: Lib Tech: skate banana')
+print ('Board 3: Nitro: prime')
+print ('Board 4: Rome: mod rocker')
+print ('Board 5: Salomon: huck knife')
+print ('Board 6: DC: maybe next board?')
+
 
