26844544ef11aaeb1af635d3865af3600ff9d82e
shao <ninedollarmilk@gmail.com>
2021-01-18 01:15:45 -0500

    v8.4

--- file1.py
+++ file1.py
@@ -6,6 +6,6 @@ print ('Board 2: Lib Tech: skate banana')
 print ('Board 3: Nitro: prime')
 print ('Board 4: Rome: mod rocker')
 print ('Board 5: Salomon: huck knife')
-print ('Board 6: DC: maybe next board?')
+print ('Board 6: DC: maybe next board? or go back to rome?')
 
 
